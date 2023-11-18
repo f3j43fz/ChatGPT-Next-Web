3decdb87180b9ab8cae4992f2d465742130936e0
adminv2pass <admin@v2pass.net>
2023-11-18 12:34:55 +0800

    update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT @Xi
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          明月皎皎照红颜，相思入梦许心间。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
