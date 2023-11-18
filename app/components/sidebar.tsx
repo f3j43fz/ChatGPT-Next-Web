757985ea3d8736e6ebc42f8fb94cfc5df6fe8956
adminv2pass <admin@v2pass.net>
2023-11-18 13:47:30 +0800

    update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT @Xi
+          ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
           明月皎皎照红颜，相思入梦许心间。
